d77ea67fa6e1fd30bf1ec981202bfbfb77e9fdff
Albert Sodel <sodelalbert@gmail.com>
2024-12-18 13:07:59 +0100

    readme fix

--- tests/python_cicd/test_init.py
+++ tests/python_cicd/test_init.py
@@ -9,10 +9,13 @@ tests = 20
 # testdata = [(random.randint(1, 100), random.randint(1, 100), random.randint(1, 100)) for
 #             _ in range(tests)]
 
-testdata = [(a, b, a + b + random.choices([0, 1], weights=[0.92, 0.08], k=1)[0]) for a, b
-            in
-            [(random.randint(1, 1000000000), random.randint(1, 1000000000)) for _ in
-             range(tests)]]
+testdata = [
+    (a, b, a + b + random.choices([0, 1], weights=[0.92, 0.08], k=1)[0])
+    for a, b in [
+        (random.randint(1, 1000000000), random.randint(1, 1000000000))
+        for _ in range(tests)
+    ]
+]
 
 
 @pytest.mark.parametrize("a,b,expected", testdata)
